55a003f8012f36476910d5c0fa6cef119719120c
Andrew Gray <andrew.russel.gray@gmail.com>
2016-06-26 22:53:15 -0500

    Re-created the General folder and its contents.

---
+++ Mesh Builder/Assets/Framework/Asvarduil.Voxels/General/WorldPosition.cs
@@ -0,0 +1,13 @@
+﻿public struct WorldPosition
+{
+    public int x;
+    public int y;
+    public int z;
+
+    public WorldPosition(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
